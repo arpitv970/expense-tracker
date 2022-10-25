4f871ad3c5eabce93c974e32c9e3ec4f3cd83eb2
Arpit Verma <arpitv970@gmail.com>
2022-10-25 22:18:00 +0530

    First commit to expense tracker

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExpenseItem from './components/ExpenseItem'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Expense Tracker</h1>
+      <ExpenseItem />
+    </div>
+  )
+}
+
+export default App
